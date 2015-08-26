686828445aa051e3ac58fcd8af94c6ddf38cb834
xiaoyue <weixiao909@gmail.com>
2015-08-26 18:10:20 +0800

    modify register funs

--- browser-require/js/rq.js
+++ browser-require/js/rq.js
@@ -1,24 +1,33 @@
-var require = function(path){
-	var p = require.resolve(path);
-	var mod = require.modules[p];
-	if(!mod){
-		throw new Error('require module is not defined');
+(function(global){
+	var require = function(path){
+		var p = require.resolve(path);
+		var mod = require.modules[p];
+		if(!mod){
+			throw new Error('require module is not defined');
+		}
+		if(!mod.exports){
+			mod.exports = {};
+			mod.call(mod.exports, mod, mod.exports, require);
+		}
+
+		return mod.exports;
+	};
+
+
+	require.resolve = function(path){
+		var orig = path;
+		return orig;
+	};
+
+	require.modules = {};
+
+	if(!global.define){
+		global.define = function(id, fn){
+			require.modules[id] = fn;
+		}
 	}
-	if(!mod.exports){
-		mod.exports = {};
-		mod.call(mod.exports, mod, mod.exports, require);
-	}
+	global.require = require;
+	
+})(this);
 
-	return mod.exports;
-};
 
-require.modules = {};
-
-require.register = function(id,fn){
-	require.modules[id] = fn;
-};
-
-require.resolve = function(path){
-	var orig = path;
-	return orig;
-};
