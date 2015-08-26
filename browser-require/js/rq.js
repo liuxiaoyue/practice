255c2273aca9ce2ee572863431972bad06e8386d
xiaoyue <weixiao909@gmail.com>
2015-08-26 10:00:36 +0800

    add prototype version

--- browser-require/js/rq.js
+++ browser-require/js/rq.js
@@ -1,5 +1,15 @@
 var require = function(path){
 	var p = require.resolve(path);
+	var mod = require.modules[p];
+	if(!mod){
+		throw new Error('require module is not defined');
+	}
+	if(!mod.exports){
+		mod.exports = {};
+		mod.call(mod.exports, mod, mod.exports, require);
+	}
+
+	return mod.exports;
 };
 
 require.modules = {};
